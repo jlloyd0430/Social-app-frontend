c5a58b46c95a39fcab0e876ffc677f6b010da3d4
jesse Lloyd <jesselloyd911@gmail.com>
2023-02-04 16:31:18 -0500

    config refactor

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://social-media-backend-2xu1.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "https://social-media-backend-2xu1.onrender.com/";
+  BASE_URL = "http://localhost:4000/";
 }
 
 export { BASE_URL };
